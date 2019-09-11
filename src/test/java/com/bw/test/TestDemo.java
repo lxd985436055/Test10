3a7888e9e2afb490fbe5353df2d18537ddf841f4
lxd <985436055@qq.com>
2019-09-11 08:43:06 +0800

    上传

---
+++ src/test/java/com/bw/test/TestDemo.java
@@ -0,0 +1,34 @@
+package com.bw.test;
+
+import org.junit.Test;
+
+import com.bw.util.DateUtil;
+
+public class TestDemo {
+
+	@Test
+	public void AgeTest() {
+		//DateUtil.getAge("2001-01-02");
+	}
+	
+	@Test
+	public void DateByMonthInitTest() {
+		//DateUtil.getDateByMonthInit("2001-01-02");
+	}
+	
+	@Test
+	public void DateByMonthLastTest() {
+		//DateUtil.getDateByMonthLast("2001-01-02");
+	}
+	
+	@Test
+	public void DaysByFutureTest() {
+		//DateUtil.getDaysByFuture("2001-01-02");
+	}
+	
+	@Test
+	public void DaysBNyDepartedTest() {
+		//DateUtil.getDaysByDeparted("2001-01-02");
+	}
+	
+}
